20a58c48e4c939a9bfadcd86b24fe0051c35da0c
Madhura Gandhe <43580898+madhuragandhe@users.noreply.github.com>
2020-06-30 16:50:37 +0530

    Update download_Youtube_video.py

--- download_Youtube_video.py
+++ download_Youtube_video.py
@@ -1,7 +1,7 @@
 #Python program to download a video from youtube
 import  pytube
 
-video_link = 'https://www.youtube.com/watch?v=XsX3ATc3FbA'
+video_link = "https://www.youtube.com/watch?v=XsX3ATc3FbA"
 yt = pytube.YouTube(video_link)
 videos = yt.streams.first()
 videos.download('R:\VIDEOS')
